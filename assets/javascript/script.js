1566cba11569fb48b0cefdffdb81f39c01d9627b
reekamaharaj <reekamaharaj@gmail.com>
2020-03-08 16:54:13 -0500

    Still need to figure out the formatting for the times. Works with firebase and things are added

---
+++ assets/javascript/script.js
@@ -0,0 +1,76 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDt54c--BF8mnjFVqRfqPgFxLk1gGGqUY8",
+    authDomain: "kittyboo-201a0.firebaseapp.com",
+    databaseURL: "https://kittyboo-201a0.firebaseio.com",
+    projectId: "kittyboo-201a0",
+    storageBucket: "kittyboo-201a0.appspot.com",
+    messagingSenderId: "1074568465141",
+    appId: "1:1074568465141:web:10b2848beaadcc04aad3bc"
+};
+
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+
+let trainName;
+let trainDestination;
+let firstTrainTime;
+let trainFrequency;
+let newTrain;
+let firstTime;
+let time;
+let minsAway;
+let nextTrain;
+let currentTime = moment();
+let nextTrainArrival;
+let timeDiff;
+
+// Capture Button Click
+$("#newTrain").on("click", function(event) {
+    event.preventDefault();
+
+    trainName = $("#trainName-input").val().trim();
+    trainDestination = $("#trainDestination-input").val().trim();
+    firstTrainTime = $("#firstTrainTime-input").val().trim();
+    trainFrequency = $("#trainFrequency-input").val().trim();
+
+    newTrain = {
+        name: trainName,
+        destination: trainDestination,
+        firstTime: firstTrainTime,
+        frequency: trainFrequency,
+    };
+    database.ref().push(newTrain);
+
+    $("#trainName-input").val("");
+    $("#trainDestination-input").val("");
+    $("#firstTrainTime-input").val("");
+    $("#trainFrequency-input").val("");
+});
+
+database.ref().on("child_added", function(snapshot){
+    trainName = snapshot.val().name;
+    trainDestination = snapshot.val().destination;
+    firstTrainTime = snapshot.val().firstTime;
+
+    firstTime = moment(firstTrainTime, "HH:mm").subtract(1, "years");
+    console.log(firstTime);
+    now = moment(currentTime).format("hh:mm");
+    time = moment().diff(moment(firstTime), "minutes");
+    console.log(time);
+    timeDiff = time % trainFrequency;
+    minsAway = trainFrequency - timeDiff;
+    console.log(minsAway);
+    nextTrainArrival = moment().add(minsAway, "minutes");
+    nextTrain = moment(nextTrainArrival).format("hh:mm");
+    $("thead").append(`
+    <tr>
+        <td>${trainName}</td>
+        <td>${trainDestination}</td>
+        <td>${trainFrequency}</td>
+        <td>${nextTrain}</td>
+        <td>${minsAway}</td>
+    </tr>
+    `)
+}, function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+})
